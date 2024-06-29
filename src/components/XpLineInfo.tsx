b61abe495b2f7e0cb6e5476e23b64e92c28b87b7
Samilly <samillynunes19@gmail.com>
2024-06-29 12:03:30 -0300

    ui: workin on xp line responsiveness

--- src/components/XpLineInfo.tsx
+++ src/components/XpLineInfo.tsx
@@ -1,15 +1,61 @@
+import { useEffect, useState } from "react";
+
 interface XpLineInfo{
     year: string;
     title: string;
     subtitle: string;
     description: string;
+    isEducation?: boolean;
 }
 
+
 export default function XpLineInfo(props: XpLineInfo){
+
+    const isEducation = props.isEducation ?? false;
+    
+    const idS1 = `s1${props.title}`;
+    const idS2 = `s2${props.title}`;
+    const idS3 = `s3${props.title}`;
+    const targetId = `target${props.title}`;
+
+    const [lineHeight, setLineHeight] = useState(100);
+
+    function adjustTextHeigth(){
+        const sourceDiv1 = document.getElementById(idS1);
+        const sourceDiv1Height = sourceDiv1?.offsetHeight;
+        const sourceDiv2 = document.getElementById(idS2);
+        const sourceDiv2Height = sourceDiv2?.offsetHeight;
+        const sourceDiv3 = document.getElementById(idS3);
+        const sourceDiv3Height = sourceDiv3?.offsetHeight;
+
+        if(sourceDiv1Height && sourceDiv2Height && sourceDiv3Height){
+            const heightsSum =  sourceDiv1Height + sourceDiv2Height + sourceDiv3Height;
+
+            // alert(heightsSum);
+
+            const targetDiv = document.getElementById(targetId);
+            if(targetDiv!=null){
+                // targetDiv.style.height = `${heightsSum}px`;
+                // alert(heightsSum.toString()+'px');
+                setLineHeight(heightsSum);
+                console.log(`line height: ${heightsSum}`)
+            }
+        }
+    }
+
+    useEffect(()=>{
+        adjustTextHeigth();
+    }, []);
+
+    window.addEventListener('resize', adjustTextHeigth);
+
     return (
         <div className="flex flex-row mt-2 items-start">
             <div className="w-[10%] flex justify-end">
-                <h3 className="text-base md:text-lg font-bold ">
+                <h3 className={`
+                    text-base md:text-lg font-bold   
+                    ${isEducation ? 'text-light-purple' : ''}   
+                `}>
                     {/* 2024 */}
                     {props.year}
                 </h3>
@@ -22,22 +68,27 @@ export default function XpLineInfo(props: XpLineInfo){
                     rounded-full
                 "></div>
 
-                <div className="
-                    w-0.5 h-72 md:h-32 
-                    bg-white
-                ">
+                <div id={targetId} 
+                    style={{height: lineHeight}}
+                    className={`
+                        w-0.5 
+                        bg-white    
+                `}>
 
                 </div>
             </div>
-            <div className="flex flex-col justify-start">
-                <h3 className="text-base md:text-xl font-bold">
+            <div className="flex flex-col justify-start w-[80%]">
+                <h3 id={idS1} className={`
+                    text-base md:text-xl font-bold 
+                    ${isEducation ? 'text-light-purple' : ''}    
+                `}>
                     {/* Analista de Desenvolvimento de Produto I */}
                     {props.title}
                 </h3>
-                <h3 className="text-base md:text-xl">
+                <h3 id={idS2} className="text-base md:text-xl">
                     {props.subtitle}
                 </h3>
-                <h5 className="text-sm md:text-base mt-5">
+                <h5 id={idS3} className="text-sm md:text-base mt-5">
                     {props.description}
                 </h5>
             </div>
--- src/components/pages/XpPage.tsx
+++ src/components/pages/XpPage.tsx
@@ -8,7 +8,7 @@ export default function XpPage(){
     return (
         <div className="h-min bg-dark-gray p-10">
             <Title label="EXPERIÊNCIA" />
-            <div className="flex flex-col mt-10">
+            <div className="flex flex-col mt-10 ">
                 <XpLineInfo
                     year="2024"
                     title="Analista de Desenvolvimento de Produto I"
@@ -16,10 +16,30 @@ export default function XpPage(){
                     description="Desenvolvedora de software focada no desenvolvimento mobile utilizando Flutter como principal ferramenta. Responsável pelo principal aplicativo do segmento, implementação e manutenção de funcionalidades, estudo de novas abordagens, participação de sprints, testes e CI/CD."
                 />
                 <XpLineInfo
+                    year="2022"
+                    title="Bacharelado em Ciência da Computação"
+                    subtitle="UFAL, Arapiraca"
+                    isEducation
+                    description="Conclusão do curso. Para além do curso, houve a participação de um projeto de desenvolvimento de Jogos para crianças autistas com Unity3D e C# (SIA); Bolsa de iniciação científica para o desenvolvimento de um sistema auxiliar para ajudar no ensino de Redes de Computadores; Bolsa de iniciação tecnológica para o desenvolvimento de um sistema inteligente para minerar e categorizar produtos de lojas do estado e auxiliar no preenchimento de NFes, no LaCCAN. Apresentação de artigos em conferência regional por dois anos (ERBASE). Prêmio de excelência acadêmica."
+                />
+                <XpLineInfo
                     year="2021"
                     title="Desenvolvimento Backend"
                     subtitle="Weni, Remoto, Estágio"
-                    description="Desenvolvedora de software focada no desenvolvimento mobile utilizando Flutter como principal ferramenta. Responsável pelo principal aplicativo do segmento, implementação e manutenção de funcionalidades, estudo de novas abordagens, participação de sprints, testes e CI/CD."
+                    description="Participação no time de Inteligência Artificial como estagiária de desenvolvimento backend utilizando Python e Django. Participação de reuniões diárias e estudos da área."
+                />
+                <XpLineInfo
+                    year="2020"
+                    title="Desenvolvimento Flutter"
+                    subtitle="Vanq, Remoto, Freelancer"
+                    description="Criação e desenvolvimento de um aplicativo no ramo financeiro para a startup Vanq. Desenvolvimento de funcionalidades e interfaces, vínculo com API externa e notificações push; participação de reuniões."
+                />
+                <XpLineInfo
+                    year="2018"
+                    title="Técnico em Informática"
+                    isEducation
+                    subtitle="IFAL, Palmeira dos Índios"
+                    description="Conclusão do ensino médio e técnico pelo Instituto Federal de Alagoas (2014-2018). Incluiu apresentação de artigo em conferência rgional; Participação de projeto de extensão focado no ensino de programação para alunos do ensino médio da rede estadual."
                 />
                 
             </div>
